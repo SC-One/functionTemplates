5488bb3fbea9c804f108f1611349865809df868d
unknown <heydartnt@gmail.com>
2020-03-29 03:24:58 -0700

    deduce in Template functions.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
+#include<string_view>
 #include<string> // for comparing between string by (><!+= etc)
+template<typename T,typename G> auto largerNumber(T a,G b)
+{
+    return a>b?a:b;
+}
 template<typename T> T largerNumber(T a,T b);
 int main()
 {
@@ -7,8 +12,10 @@ int main()
     std::cout<<largerNumber(a,b)<<std::endl;
     std::cout<<largerNumber("ab","ba")<<std::endl;
     std::string str{"Hi Guy"}, txt{"Hi Bye"};       // G>B
-    std::cout<<largerNumber(str,txt)<<std::endl;
-    //    std::cout<<largerNumber(str,"Hi Bye")<<std::endl;       //  wont compile ,in next exmaple doing it.
+    std::cout<<largerNumber(str,txt)<<std::endl;        // Now it will compile!
+    str="23456";
+    std::cout<<largerNumber(str,"12345")<<std::endl;
+    std::cout<<largerNumber(str,"34567")<<std::endl;
     return 0;
 }
 
@@ -16,3 +23,4 @@ template<typename T> T largerNumber(T a,T b)
 {
     return a>b?a:b;
 }
+
