d65a0b0ca3e50f39f2f8c250218593d6344e80af
heydar <heydar.mahmoodi75@gmail.com>
2020-03-30 02:46:28 -0400

    Function templates Overloading

--- main.cpp
+++ main.cpp
@@ -1,17 +1,37 @@
 #include<iostream>
 #include<string_view>
+#include<vector>
 #include<string> // for comparing between string by (><!+= etc)
-template<typename T> T largerNumber(T a,T b);
+template<typename T> T largerCtr(T data[],size_t count);
+template<typename T> T largerCtr(std::vector<T> data);
 int main()
 {
-    int a{10},b{20};
-    std::cout<<largerNumber<double>(a,19.9)<<std::endl;
-    std::cout<<largerNumber<double>(19.9,b)<<std::endl;
+    std::vector<double> doubleCtr{12,42,43,24,10,100,103,74,78};
+    long long llCtr[]{1029,9311,31212,47811,393178,781278,389289};
+    size_t lengthLLCtr{sizeof (llCtr)/sizeof (llCtr[0])};
+    std::vector<std::string> stringCtr{"first","second","third"};
+
+    std::cout<<largerCtr(doubleCtr)<<std::endl;
+    std::cout<<largerCtr(llCtr,lengthLLCtr)<<std::endl;
+    std::cout<<largerCtr(stringCtr)<<std::endl;
+
     return 0;
 }
 
-template<typename T> T largerNumber(T a,T b)
+template<typename T> T largerCtr(T data[],size_t count)
 {
-    return a>b?a:b;
+    if(count==1)    return data[0];
+    size_t maxIndex{};
+    for(size_t i{1};i<count;i++)
+        if(data[i]>data[maxIndex])
+            maxIndex=i;
+    return data[maxIndex];
 }
-
+template<typename T> T largerCtr(std::vector<T> data)
+{
+    T max{data.at(0)};
+    for(auto &arr:data)
+        if(arr>max)
+            max=arr;
+    return max;
+}
