308e7c311638548353a2efbfcecbed9e47874835
heydar <heydar.mahmoodi75@gmail.com>
2020-03-30 06:03:22 -0400

    Nontype template parameters.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include<iostream>
 #include<string_view>
-#include<string>
-template <typename TArg, typename TReturn=TArg>TReturn largerThing(const TArg& a, const TArg& b)
+
+template <typename Arg,char A='A',char Z='Z'> bool isInAZ(Arg& thing)
 {
-    return a>b?a:b;
+    if(thing>=A && thing<=Z)
+        return true;
+    else
+        return false;
 }
 
 int main()
 {
-    std::cout<<largerThing(123,543)<<std::endl;
+    char h{'h'};
+    std::cout<<isInAZ(h)<<std::endl;
+    h++;
+    std::cout<<isInAZ(h)<<std::endl;
+    char H='H';
+    std::cout<<isInAZ(H)<<std::endl;
+
+//    char j{'j'};
+//    std::cout<<isInAZ<'a','c'>(j)<<std::endl;             compiler is not capable to compile! next commit will correct it!
+//    char b{'b'};
+//    std::cout<<isInAZ<'a','c'>(b)<<std::endl;
     return 0;
 }
