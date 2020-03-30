310e5636b1d46e732b7b8494de0bd8abd1512ac8
heydar <heydar.mahmoodi75@gmail.com>
2020-03-30 04:06:42 -0400

    default values for template parameters.

--- main.cpp
+++ main.cpp
@@ -2,22 +2,13 @@
 #include<string_view>
 #include<string>
 
-//template<typename T1,typename T2> auto largerNumber(T1 a, T2 b)->decltype (a>b?a:b)
-//{
-//    return a>b?a:b;
-//}
-///////////////////////////////////////// instead above function:
-template <typename T1,typename T2>  decltype (auto) largerNumber(T1 a,T2 b)
+template <typename TReturn=double, typename TArg1, typename TArg2>TReturn largerThing(const TArg1& a, const TArg2& b)
 {
     return a>b?a:b;
 }
-// with this decltype(auto) you have easy way to dont write again expressions!
-// decltype(auto) syntax add in C++ 14
-
 
 int main()
 {
-    std::cout<<largerNumber(1.2,2.4)<<std::endl;
-    std::cout<<largerNumber<std::string,std::string_view>("Hey",largerNumber<std::string_view>("Bey","Cey"))<<std::endl;
+    std::cout<<largerThing(123,543)<<std::endl;
     return 0;
 }
