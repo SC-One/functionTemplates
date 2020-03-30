9280d09088a585e472b67f5af96f3d6e00f9af8d
heydar <heydar.mahmoodi75@gmail.com>
2020-03-30 03:59:12 -0400

    decltype (auto) is easy way instead of decltype(expression) that added in C++ 14

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include<iostream>
+#include<string_view>
+#include<string>
 
-template<typename T1,typename T2> auto largerNumber(T1 a, T2 b)->decltype (a>b?a:b)
+//template<typename T1,typename T2> auto largerNumber(T1 a, T2 b)->decltype (a>b?a:b)
+//{
+//    return a>b?a:b;
+//}
+///////////////////////////////////////// instead above function:
+template <typename T1,typename T2>  decltype (auto) largerNumber(T1 a,T2 b)
 {
     return a>b?a:b;
 }
+// with this decltype(auto) you have easy way to dont write again expressions!
+// decltype(auto) syntax add in C++ 14
+
+
 int main()
 {
     std::cout<<largerNumber(1.2,2.4)<<std::endl;
+    std::cout<<largerNumber<std::string,std::string_view>("Hey",largerNumber<std::string_view>("Bey","Cey"))<<std::endl;
     return 0;
 }
