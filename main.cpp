647448fc4a4b58c88c1120e3ce5077c32331406b
heydar <heydar.mahmoodi75@gmail.com>
2020-03-30 03:40:52 -0400

    Multiple typename (parameters) in function templates

--- main.cpp
+++ main.cpp
@@ -1,47 +1,13 @@
 #include<iostream>
-#include<string_view>
-#include<vector>
-#include<string>
-///////////////////////////////////////////////////////// prototype
-template<typename T> T largerCtr(T data[],size_t count);
-template<typename T> T* largerCtr(T* a,T* b);            // Pointer pass parameters.
-template<typename T> T largerCtr(std::vector<T>& data);
-/////////////////////////////////////////////////////////
+template <typename TReturn, typename TArg1, typename TArg2> TReturn larger(TArg1 a, TArg2 b)
+{
+    return a > b ? a : b;
+}
 
 int main()
 {
-    std::vector<double> doubleCtr{12,42,43,24,10,100,103,74,78};
-    long long llCtr[]{1029,9311,31212,47811,393178,781278,389289};
-    size_t lengthLLCtr{sizeof (llCtr)/sizeof (llCtr[0])};
-    std::vector<std::string> stringCtr{"first","second","third"};
-    int a{10},b{20};
-
-    std::cout<<largerCtr(doubleCtr)<<std::endl;
-    std::cout<<largerCtr(llCtr,lengthLLCtr)<<std::endl;
-    std::cout<<largerCtr(stringCtr)<<std::endl;
-    std::cout<<*largerCtr(&a,&b)<<std::endl;            // cause of that they are pointers , thus we should use derefrence operation "*" .
-
+    std::cout<<larger<double>(1.5,2.7)<<std::endl;
+    std::cout<<larger<double,size_t>(2.5,2.7)<<std::endl;
+    std::cout<<larger<double,size_t,size_t>(1.5,2.7)<<std::endl;
     return 0;
-}
-
-template<typename T> T largerCtr(T data[],size_t count)
-{
-    if(count==1)    return data[0];
-    size_t maxIndex{};
-    for(size_t i{1};i<count;i++)
-        if(data[i]>data[maxIndex])
-            maxIndex=i;
-    return data[maxIndex];
-}
-template<typename T> T largerCtr(std::vector<T>& data)
-{
-    T max{data.at(0)};
-    for(auto &arr:data)
-        if(arr>max)
-            max=arr;
-    return max;
-}
-template<typename T> T* largerCtr(T* a,T* b)
-{
-    return *a>*b?a:b;
 }
