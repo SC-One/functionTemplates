568334fede9b39ae5357e493080b4747df25a467
heydar <heydar.mahmoodi75@gmail.com>
2020-03-30 02:56:45 -0400

    Functions overloading by pointer pass.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 #include<string>
 ///////////////////////////////////////////////////////// prototype
 template<typename T> T largerCtr(T data[],size_t count);
-template<typename T> T largerCtr(std::vector<T>& data);     // refrence pass , avoid from copying vector (expensive job)!
+template<typename T> T* largerCtr(T* a,T* b);            // Pointer pass parameters.
+template<typename T> T largerCtr(std::vector<T>& data);
 /////////////////////////////////////////////////////////
 
 int main()
@@ -13,10 +14,12 @@ int main()
     long long llCtr[]{1029,9311,31212,47811,393178,781278,389289};
     size_t lengthLLCtr{sizeof (llCtr)/sizeof (llCtr[0])};
     std::vector<std::string> stringCtr{"first","second","third"};
+    int a{10},b{20};
 
     std::cout<<largerCtr(doubleCtr)<<std::endl;
     std::cout<<largerCtr(llCtr,lengthLLCtr)<<std::endl;
     std::cout<<largerCtr(stringCtr)<<std::endl;
+    std::cout<<*largerCtr(&a,&b)<<std::endl;            // cause of that they are pointers , thus we should use derefrence operation "*" .
 
     return 0;
 }
@@ -37,4 +40,8 @@ template<typename T> T largerCtr(std::vector<T>& data)
         if(arr>max)
             max=arr;
     return max;
+}
+template<typename T> T* largerCtr(T* a,T* b)
+{
+    return *a>*b?a:b;
 }
